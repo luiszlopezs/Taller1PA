28d842c98db4e2b7adb1db6332d6aed84728377b
Carlos E <sangroniscarlosenrique1@gmail.com>
2025-04-06 10:01:20 -0500

    Añadí clase Bucket, Combo, hijas de Producto

---
+++ Taller1PA/src/edu/progavud/taller1pa/modelo/Combo.java
@@ -0,0 +1,11 @@
+package edu.progavud.taller1pa.modelo;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Combo extends Producto{
+    private String saborGaseosa;
+    private List<String> extras; //Añadidos como salsas,extra-queso,adicional de chips
+    private static final int costoAgrandar = 2000; //Costo para agrandar cualquier ración de comida
+    private static final int costoExtra = 1000; //Costo por añadir cualquier extra
+    
+}
--- Taller1PA/src/edu/progavud/taller1pa/modelo/Pedido.java
+++ Taller1PA/src/edu/progavud/taller1pa/modelo/Pedido.java
@@ -3,25 +3,17 @@ package edu.progavud.taller1pa.modelo;
 import java.util.ArrayList;
 
 public class Pedido {
-    private int precio;
     private String nombre;
+    private String tipoPedido; //Para llevar o para la mesa
     private ArrayList<Producto> productos;
 
-    public Pedido(int precio, String nombre, ArrayList<Producto> productos) {
-        this.precio = precio;
+    public Pedido(String nombre, String tipoPedido, ArrayList<Producto> productos) {
         this.nombre = nombre;
+        this.tipoPedido = tipoPedido;
         this.productos = productos;
     }
    
     public Pedido() {
-    }
-
-    public int getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(int precio) {
-        this.precio = precio;
     }
 
     public ArrayList<Producto> getProductos() {
@@ -39,5 +31,20 @@ public class Pedido {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public String getTipoPedido() {
+        return tipoPedido;
+    }
+
+    public void setTipoPedido(String tipoPedido) {
+        this.tipoPedido = tipoPedido;
+    }
     
+    public int calcularPrecioTotal(){ //Método que calcula el precio de pedido, accediendo al precio de cada producto y multiplicándolo por la cantidad
+        int total = 0;
+        for(Producto p: productos){
+            total += (p.getPrecio()* p.getCantidad());
+        }
+        return total;
+    }
 }
