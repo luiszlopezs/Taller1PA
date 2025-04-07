7623a17b2ac729a3ae8aeaa061d49b818a6e424d
hailen <soyhailen@gmail.com>
2025-04-06 20:27:40 -0500

    d

--- Taller1PA/src/edu/progavud/taller1pa/modelo/Pedido.java
+++ Taller1PA/src/edu/progavud/taller1pa/modelo/Pedido.java
@@ -4,10 +4,77 @@
  */
 package edu.progavud.taller1pa.modelo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author hailen
  */
 public class Pedido {
+    
+    private String nombre;
+    private String tipoPedido;
+    private ArrayList<Producto> productos;
+    private int sumaPuntos;
+
+    public Pedido(String nombre, String tipoPedido, ArrayList<Producto> productos, int sumaPuntos) {
+        this.nombre = nombre;
+        this.tipoPedido = tipoPedido;
+        this.productos = productos;
+        this.sumaPuntos = sumaPuntos;
+    }
+
+    public Pedido() {
+    }
+    
+    public double calcularPrecioTotal(){
+        double precioTotal = 0;
+        for(Producto prod: this.productos){
+            precioTotal += (prod.getPrecio()*prod.getCantidad());
+            
+        }
+        return precioTotal;
+    }
+    
+    public int sumarPuntos(){
+        sumaPuntos = 0;
+        for (Producto producto : productos) {
+            sumaPuntos += producto.getValorPuntos();
+        }
+        return sumaPuntos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipoPedido() {
+        return tipoPedido;
+    }
+
+    public void setTipoPedido(String tipoPedido) {
+        this.tipoPedido = tipoPedido;
+    }
+
+    public ArrayList<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(ArrayList<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public int getSumaPuntos() {
+        return sumaPuntos;
+    }
+
+    public void setSumaPuntos(int sumaPuntos) {
+        this.sumaPuntos = sumaPuntos;
+    }
+    
     
 }
